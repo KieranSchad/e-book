66356aba3d66b2d439f4cd01a21a4e4cfacffeb6
Kieran Schad <kieran.schad@gmail.com>
2022-01-30 01:09:51 -0700

    tabs working

--- main.js
+++ main.js
@@ -2,76 +2,15 @@
 
 import database from "./pg_caralog_2022_01_28.json" assert { type: "json" };
 
-
-const databaseTest = [
-    {
-      "Text#": 1,
-      "Type": "Text",
-      "Issued": "1971-12-01",
-      "Title": "The Declaration of Independence of the United States of America",
-      "Language": "en",
-      "Authors": "Jefferson, Thomas, 1743-1826",
-      "Subjects": "United States -- History -- Revolution, 1775-1783 -- Sources; United States. Declaration of Independence",
-      "LoCC": "E201; JK",
-      "Bookshelves": "Politics; American Revolutionary War; United States Law"
-    },
-    {
-      "Text#": 2,
-      "Type": "Text",
-      "Issued": "1972-12-01",
-      "Title": "The United States Bill of Rights\nThe Ten Original Amendments to the Constitution of the United States",
-      "Language": "en",
-      "Authors": "United States",
-      "Subjects": "Civil rights -- United States -- Sources; United States. Constitution. 1st-10th Amendments",
-      "LoCC": "JK; KF",
-      "Bookshelves": "Politics; American Revolutionary War; United States Law"
-    },
-    {
-      "Text#": 3,
-      "Type": "Text",
-      "Issued": "1973-11-01",
-      "Title": "John F. Kennedy's Inaugural Address",
-      "Language": "en",
-      "Authors": "Kennedy, John F. (John Fitzgerald), 1917-1963",
-      "Subjects": "United States -- Foreign relations -- 1961-1963; Presidents -- United States -- Inaugural addresses",
-      "LoCC": "E838",
-      "Bookshelves": ""
-    },
-    {
-        "Text#": 4,
-        "Type": "Text",
-        "Issued": "1973-11-01",
-        "Title": "Lincoln's Gettysburg Address\nGiven November 19, 1863 on the battlefield near Gettysburg, Pennsylvania, USA",
-        "Language": "en",
-        "Authors": "Lincoln, Abraham, 1809-1865",
-        "Subjects": "Consecration of cemeteries -- Pennsylvania -- Gettysburg; Soldiers' National Cemetery (Gettysburg, Pa.); Lincoln, Abraham, 1809-1865. Gettysburg address",
-        "LoCC": "E456",
-        "Bookshelves": "US Civil War"
-      },
-      {
-        "Text#": 5,
-        "Type": "Text",
-        "Issued": "1975-12-01",
-        "Title": "The United States Constitution",
-        "Language": "en",
-        "Authors": "United States",
-        "Subjects": "United States -- Politics and government -- 1783-1789 -- Sources; United States. Constitution",
-        "LoCC": "JK; KF",
-        "Bookshelves": "United States; Politics; American Revolutionary War; United States Law"
-      },
-      {
-        "Text#": 6,
-        "Type": "Text",
-        "Issued": "1976-12-01",
-        "Title": "Give Me Liberty or Give Me Death",
-        "Language": "en",
-        "Authors": "Henry, Patrick, 1736-1799",
-        "Subjects": "Speeches, addresses, etc., American; United States -- Politics and government -- 1775-1783 -- Sources; Virginia -- Politics and government -- 1775-1783 -- Sources",
-        "LoCC": "E201",
-        "Bookshelves": "American Revolutionary War"
-      },
-]
-
+const browseButton = document.getElementById("browse-button")
+const libraryButton = document.getElementById("library-button")
+const chaptersButton = document.getElementById("chapters-button")
+const optionsButton = document.getElementById("options-button")
+const bookPanel = document.getElementById('book-panel')
+const browserPanel = document.getElementById('browser-panel')
+const libraryPanel = document.getElementById('library-panel')
+const chaptersPanel = document.getElementById('chapters-panel')
+const optionsPanel = document.getElementById('options-panel')
 
 const searchInput = document.getElementById("search-bar");
 const bookCards = document.getElementById("book-cards");
@@ -79,6 +18,7 @@ const bookCards = document.getElementById("book-cards");
 let displayBooks = [];
 
 function searchFunction(e) {
+    console.log("anything");
     const inputValue = e.target.value.toLowerCase();
     displayBooks = [];
     for (let i = 0; i < database.length; i++) {
@@ -95,7 +35,7 @@ function searchFunction(e) {
     
     const htmlString = displayBooks.map((book) => {
         return `
-        <div class="card">
+        <div id="card">
             <h1 class="title">${book.Title}</h1>
             <h2 class="subjects">${book.Subjects}</h2>
             <h3 class="author">${book.Authors}</h3>
@@ -106,28 +46,81 @@ function searchFunction(e) {
     bookCards.innerHTML = htmlString;
 }
 
+function toggleBrowse() {
+    if (browserPanel.classList.contains("active")) {
+        bookPanel.classList.add("active");
+        browserPanel.classList.remove("active");
+        browseButton.classList.remove("active");
+    } else {
+        bookPanel.classList.remove("active");
+        browserPanel.classList.add("active");
+        browseButton.classList.add("active");
+        libraryPanel.classList.remove("active");
+        libraryButton.classList.remove("active");
+        chaptersPanel.classList.remove("active");
+        chaptersButton.classList.remove("active");
+        optionsPanel.classList.remove("active");
+        optionsButton.classList.remove("active");
+    }
+}
+
+function toggleLibrary() {
+    if (libraryPanel.classList.contains("active")) {
+        bookPanel.classList.add("active");
+        libraryPanel.classList.remove("active");
+        libraryButton.classList.remove("active");
+    } else {
+        bookPanel.classList.remove("active");
+        browserPanel.classList.remove("active");
+        browseButton.classList.remove("active");
+        libraryPanel.classList.add("active");
+        libraryButton.classList.add("active");
+        chaptersPanel.classList.remove("active");
+        chaptersButton.classList.remove("active");
+        optionsPanel.classList.remove("active");
+        optionsButton.classList.remove("active");
+    }
+}
+
+function toggleChapters() {
+    if (chaptersPanel.classList.contains("active")) {
+        bookPanel.classList.add("active");
+        chaptersPanel.classList.remove("active");
+        chaptersButton.classList.remove("active");
+    } else {
+        bookPanel.classList.remove("active");
+        browserPanel.classList.remove("active");
+        browseButton.classList.remove("active");
+        libraryPanel.classList.remove("active");
+        libraryButton.classList.remove("active");
+        chaptersPanel.classList.add("active");
+        chaptersButton.classList.add("active");
+        optionsPanel.classList.remove("active");
+        optionsButton.classList.remove("active");
+    }
+}
+
+function toggleOptions() {
+    if (optionsPanel.classList.contains("active")) {
+        bookPanel.classList.add("active");
+        optionsPanel.classList.remove("active");
+        optionsButton.classList.remove("active");
+    } else {
+        bookPanel.classList.remove("active");
+        browserPanel.classList.remove("active");
+        browseButton.classList.remove("active");
+        libraryPanel.classList.remove("active");
+        libraryButton.classList.remove("active");
+        chaptersPanel.classList.remove("active");
+        chaptersButton.classList.remove("active");
+        optionsPanel.classList.add("active");
+        optionsButton.classList.add("active");
+    }
+}
 
 
-
-searchInput.addEventListener("input", searchFunction)
-
-
-// searchInput.addEventListener("input", e => {
-//     const value = e.target.value
-//     books.forEach(book => {
-//         const isVisible = book.title.includes(value) || book.subTitle.includes(value) || book.author.includes(value)
-//         book.element.classList.toggle("hide", !isVisible)
-//     })
-    
-// })
-
-// displayBooks.map(book => {
-//     const card = cardTemplate.content.cloneNode(true).children[0]
-//     const title = card.querySelector("[title]")
-//     const subTitle = card.querySelector("[subTitle]")
-//     const author = card.querySelector("[author]")
-//     title.textContent = book.Title
-//     subTitle.textContent = book.Subjects
-//     author.textContent = book.Authors
-//     bookCards.append(card)
-// })
+browseButton.addEventListener('click', toggleBrowse);
+libraryButton.addEventListener('click', toggleLibrary);
+chaptersButton.addEventListener('click', toggleChapters);
+optionsButton.addEventListener('click', toggleOptions);
+searchInput.addEventListener('input', searchFunction);
